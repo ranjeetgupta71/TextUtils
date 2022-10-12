6f1cd64dbac9ff8c7996e3a293365753eafb07ca
ranjeetgupta71 <ranjeet.github@gmail.com>
2022-10-12 20:11:32 +0530

    Adds About Us component,Enable Dark/Light Mode button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar title = "TextUtils" aboutText = "About us"/>
     <div className="container my-3">
-    <TextForm heading = "Enter the Text Heading"/>
+    {/* <TextForm heading = "Enter the Text Heading"/> */}
+    <About/>
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,6 +13,18 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText);
   }
+  const handleClearClick = ()=>{
+    let newText = '';
+    setText(newText);
+  }
+  const handleCapitaliseClick = ()=>{
+    const arr = text.split(" ");
+    for (var i = 0; i < arr.length; i++)
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    let newText = arr.join(" ");
+
+    setText(newText);
+  }
 
   const handleOnChange = (event)=>{
     setText(event.target.value)
@@ -27,6 +39,8 @@ export default function TextForm(props) {
           </div>
           <button className="btn btn-primary mx-1" onClick={handleUpClick}>ConvetToUpperCase</button>
           <button className="btn btn-primary mx-1" onClick={handleLowClick}>ConvetToLowerCase</button>
+          <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
+          <button className="btn btn-primary mx-1" onClick={handleCapitaliseClick}>Capitalise</button>
       </div>
       <div className="container my-3">
           <h2>My Text Summary</h2>
