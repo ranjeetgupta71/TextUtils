f223df959f22620f223c50db06700e1d2441e5e8
ranjeetgupta71 <ranjeet.github@gmail.com>
2022-10-26 15:56:57 +0530

    Adds dismissible Alerts

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,11 +7,24 @@ export default function TextForm(props) {
   const handleUpClick = ()=>{
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted To UpperCase","success");
+  }
+
+  const handleExtraSpace = ()=> {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
+  const handleCopy = ()=>{
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
   }
 
   const handleLowClick = ()=>{
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted To LowerCase","success");
   }
   const handleClearClick = ()=>{
     let newText = '';
@@ -41,6 +54,8 @@ export default function TextForm(props) {
           <button className="btn btn-primary mx-1" onClick={handleLowClick}>ConvetToLowerCase</button>
           <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
           <button className="btn btn-primary mx-1" onClick={handleCapitaliseClick}>Capitalise</button>
+          <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
+          <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>RemoveExtaSpace</button>
       </div>
       <div className="container my-3">
           <h2>My Text Summary</h2>
